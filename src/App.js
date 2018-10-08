a8a99aaff9100965b1cbac1a9446aeb4d7ff3ab9
Vu Cao <cdhvu@yahoo.com>
2018-10-08 12:20:46 -0700

    add petfinder api information

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
   handleTitleClick() {
