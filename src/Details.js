3aa130d7b6d23c75d2e9cf4589e876da5bf35cda
Vu Cao <cdhvu@yahoo.com>
2018-10-08 16:57:02 -0700

    Add Details/id page

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,64 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router/lib/history";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(err => {
+        navigate("/");
+      });
+  }
+
   render() {
-    return <div>Hi lol</div>;
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+    const { animal, name, location, description, media, breed } = this.state;
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+        <p>{description}</p>
+      </div>
+    );
   }
 }
 
