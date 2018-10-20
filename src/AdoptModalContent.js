5a17360da42a06fd740d5b3bd6c03e9dd03bb7cd
Vu Cao <cdhvu@yahoo.com>
2018-10-20 09:41:59 -0700

    Split code Modal

---
+++ src/AdoptModalContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AdoptModalContent = props => {
+  return (
+    <React.Fragment>
+      <h1>Would you like to adopt {props.name}?</h1>
+      <div className="buttons">
+        <button onClick={props.toggleModal}>Yes</button>
+        <button onClick={props.toggleModal}>No</button>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default AdoptModalContent;
